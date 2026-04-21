c1eee96820d65e778ef011807543c224b0b770d3
Aditya Raman <adityaarvind3@gmail.com>
2026-04-22 02:36:25 +0530

    add comments

--- RC_Watchdog.ino
+++ RC_Watchdog.ino
@@ -1,17 +1,20 @@
 #define LED 5
-#define WAKE_PIN 33
+#define WAKE_PIN 33 // ext0, single pin wakeup
 #define DISCHARGE_PIN 25
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   Serial.println("Booting...");
 
   pinMode(LED, OUTPUT);
-  pinMode(WAKE_PIN, INPUT_PULLUP);
+  pinMode(WAKE_PIN, INPUT_PULLUP); // trigger is when pin is LOW
+                                   // thus pullup
 
   // Do work
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 5; i++)
+  {
     digitalWrite(LED, HIGH);
     delay(50);
     digitalWrite(LED, LOW);
@@ -19,31 +22,27 @@ void setup() {
   }
 
   delay(50);
-  // Check wake pin BEFORE sleep
-  // Serial.print("WAKE PIN: ");
-  // Serial.println(digitalRead(WAKE_PIN));  // should be 1
 
   Serial.println("Discharging capacitor...");
 
   // Discharge RC node
-  pinMode(DISCHARGE_PIN, OUTPUT);
+  pinMode(DISCHARGE_PIN, OUTPUT); // for discharge
   digitalWrite(DISCHARGE_PIN, LOW);
-  delay(1000);  // important
-  
+  delay(1000); // important for completely letting the capacitor discharge.
 
   // Release node
   pinMode(DISCHARGE_PIN, INPUT);
-  delay(200);  // allow pin to stabilize HIGH
-  
+  delay(200); // allow pin to stabilize HIGH
+
   Serial.print("WAKE PIN: ");
-  Serial.println(digitalRead(WAKE_PIN));  // should be 1
+  Serial.println(digitalRead(WAKE_PIN)); // should be 1 as LOW is wakeup
 
   Serial.println("Sleeping...");
 
   digitalWrite(LED, LOW);
 
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_33, 0);
-  esp_deep_sleep_start();
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_33, 0); // set pin33 as ext0 i.e. single pin wakeup
+  esp_deep_sleep_start();                       // go to sleep.
 }
 
 void loop() {}
